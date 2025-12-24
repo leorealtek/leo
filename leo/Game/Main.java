54562321a09b6e99c68d28311ab058c43f90ca3a
leorealtek <leorealtek@gmail.com>
2025-12-24 17:05:17 +0100

    Finito il main

--- leo/Game/Main.java
+++ leo/Game/Main.java
@@ -20,6 +20,11 @@ public class Main {
 
         
         Weapon weaponPlayer = new Weapon(null, -1, -1);
+        Weapon mani = new Weapon("Mani", 2, -1);
+        Weapon spadaDiLegno = new Weapon("Spada di legno", 5, 10);
+        Weapon spadaDiFerro = new Weapon("Spada di ferro", 8, 17);
+        Weapon spadaLaser = new Weapon("Spada laser", 15, 35); 
+        
         boolean choose = false;
 
         while(!choose){
@@ -31,19 +36,19 @@ public class Main {
 
             switch (s.nextInt()) {
                 case 1:
-                    weaponPlayer = new Weapon("Mani", 2, -1);
+                    weaponPlayer = mani;
                     choose = true;        
                     break;
                 case 2:
-                    weaponPlayer = new Weapon("Spada di legno", 5, 10); 
+                    weaponPlayer = spadaDiLegno; 
                     choose = true;               
                     break;
                 case 3:
-                    weaponPlayer = new Weapon("Spada di ferro", 8, 17);
+                    weaponPlayer = spadaDiFerro;
                     choose = true;
                     break;
                 case 4:
-                    weaponPlayer = new Weapon("Spada laser", 15, 35);   
+                    weaponPlayer = spadaLaser;   
                     choose = true;             
                     break;
                 default:
@@ -58,8 +63,50 @@ public class Main {
         System.out.print("Dimmi il nome dell'avversario: ");
         String nameEnemy = s.nextLine();
 
-        Enemy enemy = new Enemy(nameEnemy, HEALTH_ENEMY, null);
-        System.out.println(player.toString());
-        System.out.println(enemy.toString());
+        choose = false;
+
+        Weapon weaponEnemy = new Weapon(null, -1, -1);
+
+
+        while(!choose){
+            System.out.println("Dimmi con che arma vuoi far giocare " + nameEnemy);
+            System.out.println("1) Mani");
+            System.out.println("2) Spada di legno");
+            System.out.println("3) Spada di ferro");
+            System.out.println("4) Spada laser");
+
+            switch (s.nextInt()) {
+                case 1:
+                    weaponEnemy = mani;
+                    choose = true;        
+                    break;
+                case 2:
+                    weaponEnemy = spadaDiLegno; 
+                    choose = true;               
+                    break;
+                case 3:
+                    weaponEnemy = spadaDiFerro;
+                    choose = true;
+                    break;
+                case 4:
+                    weaponEnemy = spadaLaser;   
+                    choose = true;             
+                    break;
+                default:
+                    System.out.println("Hai scelto un arma che non esiste. Riprova");
+                    Thread.sleep(700);
+                    break;
+            }
+        }
+
+        Enemy enemy = new Enemy(nameEnemy, HEALTH_ENEMY, weaponEnemy);
+        
+        while(true) {
+            if (player.getHealth() <= 0 || enemy.getHealth() <= 0) break;
+            player.attack(enemy);
+            enemy.attack(player);
+            System.out.println(player);
+            System.out.println(enemy);
+        }
     }
 }
