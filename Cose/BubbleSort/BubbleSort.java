7980b9ee4ed69ac9fb446c23da922722bb2046e5
leorealtek <leorealtek@gmail.com>
2026-01-14 17:57:03 +0100

    Sistemato bubbleSort(float e int)

--- Cose/BubbleSort/BubbleSort.java
+++ Cose/BubbleSort/BubbleSort.java
@@ -60,60 +60,29 @@ public class BubbleSort {
     }
 
     public static float[] bubbleSort(float[] array, boolean crescente) {
-        float temp;
-        boolean swapped;
-
+        double[] array2 = new double[array.length];
         for (int i = 0; i < array.length; i++) {
-            swapped = false;
-            for (int j = 0; j < array.length - i - 1; j++) {
-                if (crescente) {
-                    if (array[j] > array[j + 1]) {
-                        temp = array[j];
-                        array[j] = array[j + 1];
-                        array[j + 1] = temp;
-                        swapped = true;
-                    }
-                }
-                else {
-                    if (array[j] < array[j + 1]) {
-                        temp = array[j];
-                        array[j] = array[j + 1];
-                        array[j + 1] = temp;
-                        swapped = true;
-                    }
-                }
-            }
-            if (!swapped) break;
+            array2[i] = (double) array[i];
+        }
+        array2 = bubbleSort(array2, crescente);
+
+        for (int i = 0; i < array2.length; i++) {
+            array[i] = (float) array2[i];
         }
         return array;
     }
 
     public static int[] bubbleSort(int[] array, boolean crescente) {
-        int temp;
-        boolean swapped;
-
+        long[] array2 = new long[array.length];
         for (int i = 0; i < array.length; i++) {
-            swapped = false;
-            for (int j = 0; j < array.length - i - 1; j++) {
-                if (crescente) {
-                    if (array[j] > array[j + 1]) {
-                        temp = array[j];
-                        array[j] = array[j + 1];
-                        array[j + 1] = temp;
-                        swapped = true;
-                    }
-                }
-                else {
-                    if (array[j] < array[j + 1]) {
-                        temp = array[j];
-                        array[j] = array[j + 1];
-                        array[j + 1] = temp;
-                        swapped = true;
-                    }
-                }
-            }
-            if (!swapped) break;
+            array2[i] = (long) array[i];
+        }
+        array2 = bubbleSort(array2, crescente);
+
+        for (int i = 0; i < array2.length; i++) {
+            array[i] = (int) array2[i];
         }
         return array;
     } 
+
 }
