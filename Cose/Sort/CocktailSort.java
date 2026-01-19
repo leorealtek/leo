59f62c0ef682ef5bd11ab415c93db6a3ea1e2e56
leoreltek <leorealtek@gmail.com>
2026-01-19 12:03:13 +0100

    Fatto il cocktailSort

--- Cose/Sort/CocktailSort.java
+++ Cose/Sort/CocktailSort.java
@@ -1,5 +1,36 @@
 package Sort;
 
 public class CocktailSort {
+    public static int[] cocktailSort (int[] array) {
+        boolean swapped = true;
+        int start = 0;
+        int end = array.length - 1;
+        int temp;
     
+        while (swapped) {
+            swapped = false;
+            for (int i = start; i < end; i++) {
+                if (array[i] > array[i + 1]) {
+                    temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) break;
+            end--;
+            swapped = false;
+
+            for (int i = end; i > start; i--) {
+                if (array[i] < array[i - 1]) {
+                    temp = array[i];
+                    array[i] = array[i - 1];
+                    array[i - 1] = temp;
+                    swapped = true;
+                }
+            }
+            start++;
+        }
+        return array;
+    }
 }
--- Cose/Sort/Esercizio.java
+++ Cose/Sort/Esercizio.java
@@ -1,10 +1,8 @@
 package Sort;
 
 import java.util.Random;
-import java.util.Scanner;
 
 public class Esercizio {
-    private static Scanner s = new Scanner(System.in);
 
     public static int[] creaArray(int n, int min, int max) {
         int[] array = new int[n];
@@ -15,7 +13,7 @@ public class Esercizio {
         return array;
     }
 
-    public static void provaTempoEsecuzione() {
+    public static void provaTempoEsecuzioneBubble() {
         int length = 10;
         for (int i = 0; i < 5; i++) {
             int[] array = creaArray(length, 0, 100);
@@ -27,7 +25,20 @@ public class Esercizio {
         }
     }
 
+    public static void provaTempoEsecuzioneCocktail() {
+        int length = 10;
+        for (int i = 0; i < 5; i++) {
+            int[] array = creaArray(length, 0, 100);
+            long startTime = System.nanoTime();
+            CocktailSort.cocktailSort(array);
+            long endTime = System.nanoTime();
+            System.out.println("Elementi: " + length + " Tempo: " + (endTime - startTime) + " ns");
+            length *= 10;
+        }
+    }
+
     public static void main(String[] args) {
-        provaTempoEsecuzione();
+        provaTempoEsecuzioneBubble();
+        provaTempoEsecuzioneCocktail();
     }
 }
