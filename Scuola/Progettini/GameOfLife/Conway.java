fd68a1f3ed2a1bdb40a075567c19890d0223f134
leorealtek <leorealtek@gmail.com>
2026-02-04 16:12:44 +0100

    Fix

--- Scuola/Progettini/GameOfLife/Conway.java
+++ Scuola/Progettini/GameOfLife/Conway.java
@@ -3,12 +3,21 @@ package Scuola.Progettini.GameOfLife;
 import java.util.Scanner;
 
 public class Conway {
-    private static char[][] mappa;
-    private static final Scanner kbd = new Scanner(System.in);
-    private static int comando;
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
+    public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+    public static final Scanner kbd = new Scanner(System.in);
 
-    public static char[][] creaMappa(){
-        char[][] mappa = new char[30][30];
+    private final int larghezza;
+    private char[][] mappa;
+
+    public Conway(int larghezza) {
+        this.larghezza = larghezza;
+        this.mappa = creaMappa();
+    }
+
+    private char[][] creaMappa(){
+        char[][] mappa = new char[larghezza][larghezza];
         for (int i = 0; i < mappa.length; i++) {
             for (int j = 0; j < mappa[i].length; j++) {
                 mappa[i][j] = '.';
@@ -17,21 +26,25 @@ public class Conway {
         return mappa;
     }
 
-    public static void stampaMappa(char[][] mappa) {
+    public void stampaMappa() {
         for (int i = 0; i < mappa.length; i++) {
             for (int j = 0; j < mappa[i].length; j++) {
-                System.out.print(mappa[i][j] + " ");
+                if (mappa[i][j] == 'V') {
+                    System.out.print(ANSI_GREEN_BACKGROUND + " " + ANSI_RESET);
+                } else {
+                    System.out.print(ANSI_RED_BACKGROUND + " " + ANSI_RESET);
+                }
             }
-            System.err.println("");
+            System.out.println();
         }
     }
 
-    public static boolean stampaCelleVive(char[][] mappa){
+    public void stampaCelleVive(){
         int indice = 1;
         System.out.println("Celle vive:");
-        for (int col = 0; col < mappa.length; col++) {
-            for (int riga = 0; riga < mappa[col].length; riga++) {
-                if (mappa[col][riga] == 'V') {
+        for (int riga = 0; riga < mappa.length; riga++) {
+            for (int col = 0; col < mappa[riga].length; col++) {
+                if (mappa[riga][col] == 'V') {
                     System.out.println(indice + ". Riga: " + riga + " colonna: " + col);
                     indice++;
                 }
@@ -39,33 +52,35 @@ public class Conway {
         }
         if (indice == 1) {
             System.out.println("non ci sono celle vive");
-            return false;
         }
-        return true;
     }
     
-    public static void mettiVita(char[][] mappa, int col , int riga){
+    public void mettiVita(int riga, int col){
         try {
-            mappa[col][riga] = 'V';
-        } catch (Exception e) {
+            mappa[riga][col] = 'V';
+        } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Coordinate invalide");
         }
     }
 
-    public static void togliVita(char[][] mappa, int col , int riga){
+    public void togliVita(int riga, int col){
         try {
-            mappa[col][riga] = '.';
-        } catch (Exception e) {
+            mappa[riga][col] = '.';
+        } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Coordinate invalide");
         }
     }
 
-    public static int contaViciniVivi(char[][] mappa , int col , int riga){
-        int viciniVIvi = 0;
+    public int contaViciniVivi(int riga, int col){
+        int viciniVivi = 0;
         boolean esiste;
         char contenutoCella = ' ';
-        for (int k = col-1; k <= col+1; k++) {
-            for (int m = riga-1; m <= riga + 1; m++) {
+        for (int k = riga-1; k <= riga+1; k++) {
+            for (int m = col-1; m <= col + 1; m++) {
+                if (k == riga && m == col) {
+                    continue;
+                }
+                
                 esiste = true;
                 try {
                     contenutoCella = mappa[k][m];
@@ -74,91 +89,86 @@ public class Conway {
                 }
                 if (esiste) {
                     if (contenutoCella == 'V') {
-                        viciniVIvi++;
+                        viciniVivi++;
                     } 
                 }
             }
         }
-        return viciniVIvi;
+        return viciniVivi;
     }
 
-    public static void muore(char[][] mappa , int col , int riga){
-        int viciniVIvi = contaViciniVivi(mappa, col, riga);
-        if (viciniVIvi < 2) {
-            mappa[col][riga] = '.';
-        }
-        else if (viciniVIvi > 3) {
-            mappa[col][riga] = '.';
-        }
-    }
-
-    public static void nasce(char[][] mappa , int col , int riga){
-        int viciniVIvi = contaViciniVivi(mappa, col, riga);
-        if (viciniVIvi == 3){
-            mappa[col][riga] = 'V';
-        }
-    }
-
-    public static void round(char[][] mappa){
-        for (int col = 0; col < mappa.length; col++) {
-            for (int riga = 0; riga < mappa[col].length; riga++) {
-                if (mappa[col][riga] == 'V') {
-                    muore(mappa, col, riga);
-                }
-                else{
-                    nasce(mappa, col, riga);
+    public void round(){
+        char[][] nuovaMappa = new char[larghezza][larghezza];
+        
+        for (int riga = 0; riga < mappa.length; riga++) {
+            for (int col = 0; col < mappa[riga].length; col++) {
+                int viciniVivi = contaViciniVivi(riga, col);
+                
+                if (mappa[riga][col] == 'V') {
+                    if (viciniVivi == 2 || viciniVivi == 3) {
+                        nuovaMappa[riga][col] = 'V';
+                    } else {
+                        nuovaMappa[riga][col] = '.';
+                    }
+                } else {
+                    if (viciniVivi == 3) {
+                        nuovaMappa[riga][col] = 'V';
+                    } else {
+                        nuovaMappa[riga][col] = '.';
+                    }
                 }
             }
         }
+        
+        mappa = nuovaMappa;
     }
+    
     public static void main(String[] args) {
-        mappa = creaMappa();
-        int riga;
-        int colonna;
-        int round;
+        int riga, colonna, round, comando;
         
         System.out.println("\n----- Benvenuto al Conway game of life -----\n"); 
+        System.out.print("Inserisci la dimensione della mappa per iniziare: ");
+        int larghezza = kbd.nextInt();
+        Conway game = new Conway(larghezza);
+        System.out.println("\nComandi disponibili:");
         System.out.println("1. Aggiungi casella viva");
         System.out.println("2. Rimuovi casella viva");
-        System.out.println("3. inizia la partita");
-        System.out.print("\nComando:");
+        System.out.println("3. Inizia la partita");
+        System.out.print("\nComando: ");
         comando = kbd.nextInt();
 
         while (comando != 3) {
             if (comando < 1 || comando > 3) {
-                System.out.println("errore comando invalido");
+                System.out.println("Errore comando invalido");
             }
             else if (comando == 1){
-                System.out.print("inserisci la colonna della cella viva (0-29):");
-                colonna = kbd.nextInt();
-                String boh = kbd.nextLine();
-                System.out.println(boh);
-                System.out.print("inserisci la riga della cella viva (0-29):");
+                System.out.print("Inserisci la riga della cella viva (0-" + (larghezza-1) + "): ");
                 riga = kbd.nextInt();
-                mettiVita(mappa, colonna, riga);
+                System.out.print("Inserisci la colonna della cella viva (0-" + (larghezza-1) + "): ");
+                colonna = kbd.nextInt();
+                game.mettiVita(riga, colonna);
             }
             else {
-                if (stampaCelleVive(mappa)){
-                    System.out.print("inserisci la colonna della cella viva da rimuovere (0-29):");
-                    colonna = kbd.nextInt();
-                    String boh = kbd.nextLine();
-                    System.out.println(boh);
-                    System.out.print("inserisci la riga della cella viva da rimuovere (0-29):");
-                    riga = kbd.nextInt();
-                    togliVita(mappa, colonna, riga);
+                System.out.print("Inserisci la riga della cella viva da rimuovere (0-" + (larghezza-1) + "): ");
+                riga = kbd.nextInt();
+                System.out.print("Inserisci la colonna della cella viva da rimuovere (0-" + (larghezza-1) + "): ");
+                colonna = kbd.nextInt();
+                if (game.mappa[riga][colonna] != 'V') {
+                    System.out.println("Errore: la cella selezionata non e' viva");
+                    continue;
                 }
+                game.togliVita(riga, colonna);
             }
-            System.out.print("inserire nuovo comando:");
-            comando= kbd.nextInt();
+            System.out.print("Inserire nuovo comando: ");
+            comando = kbd.nextInt();
         }
-        System.out.print("Inserisci numero di round da eseguire:");
-        round = kbd.nextInt();
-        System.out.println("\n------- mappa iniziale -------\n");
-        stampaMappa(mappa);
-        for (int i = 1; i <= round; i++) {
-            System.out.println("\n------- round numero " + i + " -------\n");
-            round(mappa);
-            stampaMappa(mappa);
+        System.out.print("Inserisci numero di round da eseguire: ");
+        round = kbd.nextInt(); 
+        for (int i = 0; i <= round; i++) {
+            System.out.println("\n------- Mappa al round " + i + " -------\n");
+            game.round();
+            game.stampaMappa();
         }
+        kbd.close();
     }
 }
