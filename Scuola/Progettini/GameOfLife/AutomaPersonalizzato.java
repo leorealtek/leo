c6caacac183430a07046327678a5a36db2a2b07f
leoreltek <leorealtek@gmail.com>
2026-02-06 12:11:06 +0100

    fix

--- Scuola/Progettini/GameOfLife/AutomaPersonalizzato.java
+++ Scuola/Progettini/GameOfLife/AutomaPersonalizzato.java
@@ -8,13 +8,6 @@ public class AutomaPersonalizzato extends Automa1D {
     @Override
     protected String calcolaStato(char sinistra, char centro, char destra) {
         if (sinistra == '1' && centro == '1' && destra == '1') return "0";
-        else if (sinistra == '1' || centro == '1' || destra == '0') return "1";
-        else if (sinistra == '1' || centro == '0' && destra == '1') return "1";
-        else if (sinistra == '1' && centro == '0' || destra == '0') return "1";
-        else if (sinistra == '1' && centro == '0' && destra == '0') return "1";
-        else if (sinistra == '0' && centro == '1' && destra == '1') return "1";
-        else if (sinistra == '0' || centro == '1' && destra == '0') return "0";
-        else if (sinistra == '0' && centro == '0' && destra == '1') return "0";
         else return "1";
     }
 }
